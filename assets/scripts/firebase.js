a17bd931aa1f9b5132efa48eba47dc8bf9e6b5ea
Candace Johnson <ccj286@nyu.edu>
2018-08-11 01:33:25 -0400

    testing form submit

--- assets/scripts/firebase.js
+++ assets/scripts/firebase.js
@@ -1,14 +1,48 @@
 // TODO: LINK TO FIREBASE AND SEND FORM INFORMATION TO IT
 
-var mykey = config.key;
-console.log(mykey);
+let form = document.getElementById('entryForm');
 
-// Initialize an instance of Firebase
-firebase.initializeApp({
-  apiKey: '### FIREBASE API KEY ###',
-  authDomain: '### FIREBASE AUTH DOMAIN ###',
-  projectId: '### CLOUD FIRESTORE PROJECT ID ###'
-});
+submitData = () => {
+  // Initialize an instance of Firebase
+  firebase.initializeApp(config);
 
-// Initialize Cloud Firestore through Firebase
-var db = firebase.firestore();
+  // Initialize Cloud Firestore through Firebase
+  var db = firebase.firestore();
+
+  // Add a new document with a generated id.
+  db.collection("userData").add({
+      name: '',
+      address: '',
+      originCountries: [],
+      category: '',
+      google: false,
+      otherInfo: '',
+      telephoneNum: 1234567890
+  })
+  .then(function(docRef) {
+      console.log("Document written with ID: ", docRef.id);
+  })
+  .catch(function(error) {
+      console.error("Error adding document: ", error);
+  });
+}
+
+form.onsubmit = () => {
+  let name = document.getElementById('name').value;
+  let address = document.getElementById('address').value;
+  let originCountries = $('#originCountries').val();
+  let category = document.getElementById('category').value;
+  let google = false;
+  var radios = document.getElementsByName('online');
+
+  for (var i = 0, length = radios.length; i < length; i++) {
+     if (radios[i].checked) {
+      google = radios[i].value;
+      break;
+     }
+  }
+
+  let otherInfo = document.getElementById('moreInfo').value;
+  let telephone = document.getElementById('telephone').value;
+
+};
