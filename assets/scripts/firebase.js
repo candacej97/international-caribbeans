10e2e22a81b61c36210618811d7ad8e060e07736
Candace Johnson <ccj286@nyu.edu>
2018-08-11 01:56:20 -0400

    connected firebase (kinda)

--- assets/scripts/firebase.js
+++ assets/scripts/firebase.js
@@ -2,38 +2,13 @@
 
 let form = document.getElementById('entryForm');
 
-submitData = () => {
-  // Initialize an instance of Firebase
-  firebase.initializeApp(config);
-
-  // Initialize Cloud Firestore through Firebase
-  var db = firebase.firestore();
-
-  // Add a new document with a generated id.
-  db.collection("userData").add({
-      name: '',
-      address: '',
-      originCountries: [],
-      category: '',
-      google: false,
-      otherInfo: '',
-      telephoneNum: 1234567890
-  })
-  .then(function(docRef) {
-      console.log("Document written with ID: ", docRef.id);
-  })
-  .catch(function(error) {
-      console.error("Error adding document: ", error);
-  });
-}
-
 form.onsubmit = () => {
   let name = document.getElementById('name').value;
   let address = document.getElementById('address').value;
   let originCountries = $('#originCountries').val();
   let category = document.getElementById('category').value;
   let google = false;
-  var radios = document.getElementsByName('online');
+  let radios = document.getElementsByName('online');
 
   for (var i = 0, length = radios.length; i < length; i++) {
      if (radios[i].checked) {
@@ -44,5 +19,29 @@ form.onsubmit = () => {
 
   let otherInfo = document.getElementById('moreInfo').value;
   let telephone = document.getElementById('telephone').value;
+
+
+  // Initialize an instance of Firebase
+  firebase.initializeApp(config);
+
+  // Initialize Cloud Firestore through Firebase
+  var db = firebase.firestore();
+
+  // Add a new document with a generated id.
+  db.collection("userData").add({
+      name: name,
+      address: address,
+      originCountries: originCountries,
+      category: category,
+      google: google,
+      otherInfo: otherInfo,
+      telephoneNum: telephone
+  })
+  .then(function(docRef) {
+      console.log("Document written with ID: ", docRef.id);
+  })
+  .catch(function(error) {
+      console.error("Error adding document: ", error);
+  });
 
 };
