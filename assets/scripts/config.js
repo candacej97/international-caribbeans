95913c957c99e1b8653c26a63527c6554442fae6
Candace Johnson <ccj286@nyu.edu>
2018-08-13 16:52:17 -0400

    minor commit

---
+++ assets/scripts/config.js
@@ -0,0 +1,11 @@
+let firebaseConfig = {
+  apiKey: "AIzaSyAEBTCYxkVWag_DlBtjV3iqFUS3nczfZM4",
+  authDomain: "international-caribbeans-f0ccf.firebaseapp.com",
+  databaseURL: "https://international-caribbeans-f0ccf.firebaseio.com",
+  // projectId: "international-caribbeans-f0ccf",
+  storageBucket: "international-caribbeans-f0ccf.appspot.com",
+  // messagingSenderId: "846539000806"
+}
+let mapfitConfig = {
+  apiKey: "591dccc4e499ca0001a4c6a42160d501d8274694922bc9b9b158057f"
+}
