d866ddff6b7f78503d871617c4e6ec2985c98907
Candace Johnson <ccj286@nyu.edu>
2018-08-13 18:09:26 -0400

    "var" to "let"

--- assets/scripts/firebase.js
+++ assets/scripts/firebase.js
@@ -1,7 +1,7 @@
 // Code below is from https://gist.github.com/dvidsilva/e000acc9610b21e43e0a58e5982bd6e9
 
 (function(){
-    var newscript = document.createElement('script');
+    let newscript = document.createElement('script');
        newscript.type = 'text/javascript';
        newscript.async = true;
        newscript.src = 'https://www.gstatic.com/firebasejs/3.0.2/firebase.js';
@@ -10,7 +10,7 @@
 
 _setFormData = function setFormData (sel, data) {
     console.info('setting form to data', data);
-    var inputList = document.querySelectorAll(sel + ' [name]');
+    let inputList = document.querySelectorAll(sel + ' [name]');
     [].forEach.call(inputList, function(input) {
         console.log(input);
         if (data[input.name] && data[input.name] !== "undefined") {
@@ -19,8 +19,8 @@ _setFormData = function setFormData (sel, data) {
     });
 };
 
-var _fb;
-var fbToForm = function fbToForm (key, sel) {
+let _fb;
+let fbToForm = function fbToForm (key, sel) {
   _fb = _fb && _fb.name === "fbToForm" ? _fb : firebase.initializeApp(firebaseConfig, "fbToForm");
   _fb.database().ref('user-data/' + key).on('value', function(snapshot) {
       _setFormData(sel, snapshot.val());
