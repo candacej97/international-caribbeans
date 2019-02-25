a1f21c087f01bead7b8b80f1e6c55a6c987e9caf
Candace Johnson <ccj286@nyu.edu>
2019-02-25 14:55:10 -0500

    replaced mapfit map with mapbox

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -6,6 +6,3 @@ let firebaseConfig = {
   storageBucket: "international-caribbeans-f0ccf.appspot.com",
   // messagingSenderId: "846539000806"
 }
-let mapfitConfig = {
-  apiKey: "591dccc4e499ca0001a4c6a42160d501d8274694922bc9b9b158057f"
-}
